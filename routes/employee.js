91890dee2a786a853489e2f8dff2113adaed1f0b
Karlo Paz <kpaz30@alumnos.uaq.mx>
2022-12-01 14:17:08 -0600

    Modifed frontend logic

--- routes/employee.js
+++ routes/employee.js
@@ -6,7 +6,7 @@ const db = require('../config/database')
 employee.post('/',async(req,res,next)=>{
     const {idEmpleado,nombreEmpleado,apellidosEmpleado,telefonoEmpleado,correoEmpleado,direccionEmpleado}=req.body
         if (idEmpleado&&nombreEmpleado&&apellidosEmpleado&&telefonoEmpleado&&correoEmpleado&&direccionEmpleado){
-            let query = `INSERT INTO empleados(idEmpleado,nombreEmpleado,apellidosEmpleado, telefonoEmpleado, correoEmpleado, direccionEmpleado) VALUES (${idEmpleado},'${nombreEmpleado}','${apellidosEmpleado}',${telefonoEmpleado},'${correoEmpleado}','${direccionEmpleado}')`
+            let query = `INSERT INTO empleados(idEmpleado,nombreEmpleado,apellidosEmpleado, telefonoEmpleado, correoEmpleado, direccionEmpleado) VALUES (${idEmpleado},'${nombreEmpleado}','${apellidosEmpleado}','${telefonoEmpleado}','${correoEmpleado}','${direccionEmpleado}')`
         const rows = await db.query(query)
         if (rows.affectedRows == 1){
             return res.status(201).json({code:200, message: 'Empleado insertado correctamente'})
@@ -33,7 +33,7 @@ employee.put("/:id([0-9]{1,3})",async(req,res,next)=>{
     const {nombreEmpleado,apellidosEmpleado,telefonoEmpleado,correoEmpleado,direccionEmpleado} = req.body
     if (nombreEmpleado&&apellidosEmpleado&&telefonoEmpleado&&correoEmpleado&&direccionEmpleado){
         let query = `UPDATE empleados SET nombreEmpleado='${nombreEmpleado}',apellidosEmpleado='${apellidosEmpleado}',`
-        query += `telefonoEmpleado=${pok_weight},correoEmpleado='${correoEmpleado}',direccionEmpleado='${direccionEmpleado}' WHERE idEmpleado=${req.params.id};`        
+        query += `telefonoEmpleado='${telefonoEmpleado}',correoEmpleado='${correoEmpleado}',direccionEmpleado='${direccionEmpleado}' WHERE idEmpleado=${req.params.id};`        
         const rows = await db.query(query)
         if (rows.affectedRows == 1){
             return res.status(200).json({code:200, message: 'Empleado actualizado correctamente'})
